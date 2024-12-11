ea253bdbbd80a8c64fe5b13e955eb2c8cea40a58
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2024-12-11 21:17:05 +0530

    Add files via upload
    input

---
+++ 03.py
@@ -0,0 +1,12 @@
+#Taking input from user & printing it
+#string-
+name = input("name :")
+print(name)
+
+#int-
+age = int(input("age:"))
+print(age)
+
+#float-
+price = float(input("price:"))
+print(price)
